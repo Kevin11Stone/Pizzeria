3f6937fcf9ecd4175e7d1f1095e61d5d1742b75d
Kevin Stone <ruca6616@gmail.com>
2020-01-09 19:33:05 -0800

    Created last of the intial models: CustomPizza and Order

---
+++ Pizzeria/Models/InitialModels/CustomPizza.cs
@@ -0,0 +1,26 @@
+﻿using Pizzeria.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pizzeria.Models.InitialModels
+{
+
+    public class CustomPizza : IMenuItem
+    {
+        string defaultCustomPizzaName = "Custom Pizza";
+        private string _name;
+
+        /// <summary>
+        /// Always a default name
+        /// </summary>
+        public string ItemName {
+            get => _name;
+            set => _name = defaultCustomPizzaName;
+        }
+        public string ItemDescription { get; set; }
+        public string Category { get; set; }
+        public decimal ItemPrice { get; set; }
+    }
+}
---
+++ Pizzeria/Models/InitialModels/Order.cs
@@ -0,0 +1,52 @@
+﻿using Pizzeria.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pizzeria.Models.InitialModels
+{
+    public class Order
+    {
+
+        public Order()
+        {
+            this.OrderItems = new List<IMenuItem>();
+        }
+
+        public int OrderId { get; set; }
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal total = 0;
+                foreach (IMenuItem item in this.OrderItems)
+                {
+                    total += item.ItemPrice;
+                }
+                return total;
+            }
+            set
+            {
+                TotalPrice = value;
+            }
+        }
+
+        /// <summary>
+        /// List of items in a given order.
+        /// </summary>
+        public ICollection<IMenuItem> OrderItems { get; set; }
+
+
+        /// <summary>
+        /// Adds menu item to list of items for an order and 
+        /// adds the price of the item to the order's total price.
+        /// </summary>
+        /// <param name="menuItem">The menu item to add the order</param>
+        public void AddItemToOrder(IMenuItem menuItem)
+        {
+            this.OrderItems.Add(menuItem);
+            this.TotalPrice += menuItem.ItemPrice;
+        }
+    }
+}
