755214134b501e60b8032ff212a8a82ad60b3a98
Kevin Stone <ruca6616@gmail.com>
2020-01-15 16:28:54 -0800

    Created database folder and menu class to be implemented.

---
+++ Pizzeria/Database/MenuDb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Contains helper methods for menu items (CRUD)
+/// </summary>
+namespace Pizzeria.Database
+{
+    public class MenuDb
+    {
+
+    }
+}
